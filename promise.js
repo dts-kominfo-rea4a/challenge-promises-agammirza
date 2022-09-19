aa70aa60a7cd183247e30e81aea8f89ffd341540
agammirza <mirza.46mz@gmail.com>
2022-09-19 21:53:46 +0700

    feat : Promise & Async Await - menghitung jumlah penonton film hasil marah dan tidak marah

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const semuaTheater = theaterIXX.concat(theaterVGC)
+
+  let countEmosi = 0;
+
+  semuaTheater.forEach((film) => {
+    if (film.hasil === emosi) {
+      countEmosi++;
+    }
+  })
+
+  return countEmosi;
+
+};
+
+console.log(promiseOutput('marah'))
+
+module.exports = {
+  promiseOutput,
+};
 
 module.exports = {
   promiseOutput,
